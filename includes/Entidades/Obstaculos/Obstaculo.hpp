d1c2df467fe0b15b3b0e587e7386e5e80a27147e
juliatorres82 <163934392+juliatorres82@users.noreply.github.com>
2024-08-25 15:24:54 -0300

    Create Obstaculo.hpp

---
+++ includes/Entidades/Obstaculos/Obstaculo.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../../Entidades/Personagens/Jogador.h"
+
+namespace Entidades
+{
+	namespace Obstaculos
+	{
+		class Obstaculo: public Entidade
+		{
+			protected:
+				bool danoso;
+
+			public:
+				Obstaculo(bool daDano = false);
+				~Obstaculo();
+
+				virtual void executar() = 0;
+				virtual void obstacular() = 0;
+				void empuxo();
+
+		};
+	}
+}

f7944fad5233d6b278980d64a84fe78fbc7e589a
juliatorres82 <163934392+juliatorres82@users.noreply.github.com>
2024-08-25 15:28:00 -0300

    Create Fase.h

---
+++ includes/Fase/Fase.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "../Gerenciadores/Gerenciador_Colisoes.h"
+#include "../Listas/ListaEntidades.h"
+#include "../Gerenciadores/Gerenciador_Eventos.h"
+#include "../Entidades/Obstaculos/Plataforma.h"
+#include "../Entidades/Personagens/Fantasma.h"
+
+using namespace Listas;
+
+namespace Fases {
+
+class Fase : public Ente {
+
+    protected:
+
+        ListaEntidades listaJogadores;
+        ListaEntidades listaInimigos;
+        ListaEntidades listaObstaculos;
+        ListaEntidades listaPlataforma;
+
+        Gerenciador_Grafico* p_GG;
+        Gerenciador_Eventos* p_GE;
+        Gerenciador_Colisoes* p_GC;
+
+    public:
+
+        Fase(); //
+        ~Fase();
+
+        void criaJogadores();
+        void criaInimigos();
+        void criaObstaculos();
+        void criaPlataforma();
+
+        void lerJSON(); //
+        void criaFundo(); //
+        void criaMapa();  //
+
+        void tratarColisoes();
+        void tratarEventos(); //
+
+        void desenhar(); 
+};
+
+}

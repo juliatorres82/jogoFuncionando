5fbecd34988ee704441a0c4c23694147e741c4fb
juliatorres82 <163934392+juliatorres82@users.noreply.github.com>
2024-08-25 15:17:25 -0300

    Create Obstaculo.cpp

---
+++ src/Entidades/Obstaculos/Obstaculo.cpp
@@ -0,0 +1,18 @@
+#include "../../../includes/Entidades/Obstaculos/Obstaculo.h"
+using namespace Entidades::Obstaculos;
+
+Obstaculo::Obstaculo(bool daDano)
+{
+	tipo = estatico;
+	id = idEntes::obsTaculo;
+	danoso = daDano;
+}
+
+Obstaculo::~Obstaculo()
+{
+}
+
+void Obstaculo::empuxo()
+{
+	empuxo();
+}
